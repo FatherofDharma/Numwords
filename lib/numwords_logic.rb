638fb6a61037bf86c3a15abefae2387a5b66d8ed
Daniel Hongel and Jessica Doss <jessica@>
2019-07-16 16:26:26 -0700

    solve the simple number solution but does not do math to break the number down into multiple words. needs the hash order reversed.

--- lib/numwords_logic.rb
+++ lib/numwords_logic.rb
@@ -10,14 +10,22 @@ class NumberConverter
   # end
 
   def wordResponse
-    @list = {
-    1 => "one", 2 => "two", 3 => "three", 4 => "four", 5 => "five", 6 => "six", 7 => "svennness", 8 => "eight", 9 => "nine", 10 =>"ten"
-    }
+    @list = { 1000000000 => "trillion", 1000000 => "million", 1000 => "thousand", 100 => "hundred", 90 => "ninety",  80 => "eighty", 70 => "seventy", 60 => "sixty", 50 => "fifty", 40 => "forty", 30 => "thirty", 20 => "twenty", 19 => "nineteen", 18 => "eighteen", 17 => "seventeen", 16 => "sixteen", 15 => "fifteen", 14 => "fourteen", 13 => "thirteen", 12 => "twelve", 11 => "eleven", 10 => "ten", 9 => "nine", 8 => "eight", 7 => "seven", 6 => "six", 5 => "five", 4 => "four", 3 => "three", 2 => "two", 1 => "one"}
     @word_array = []
-    if @number == 8
-      @word_array.push(@list.fetch(8))
+    x = 0
+    position = @list.keys
+    until (x > 2)
+  binding.pry
+      if @number/(position[x]) > 0
+        @number /=(position[x])
+        @word_array.push(@list[position[x]])
+
+        print @number
+      elsif @number%(position[x]) == 0
+        @word_array.push(@list[position[x]])
+      end
+      x = x + 1
     end
-    binding.pry
     print @word_array
     @word_array[0]
   end
--- spec/numwords_logic_spec.rb
+++ spec/numwords_logic_spec.rb
@@ -3,9 +3,23 @@ require('numwords_logic')
 require('pry')
 
 describe('NumberConverter') do
+
   it('takes a number from the user and returns it in words') do
     user_input = NumberConverter.new(8)
     expect(user_input.wordResponse).to(eq(
       "eight"))
   end
+
+  it('takes a number from the user and returns it in words') do
+    user_input = NumberConverter.new(80)
+    expect(user_input.wordResponse).to(eq(
+      "eighty"))
+  end
+
+  it('takes a number from the user and returns it in words') do
+    user_input = NumberConverter.new(10)
+    expect(user_input.wordResponse).to(eq(
+      "ten"))
+  end
+
 end

595875655ee48e77707ff76a6890af49e8e5b479
Daniel Hongel and Jessica Doss <daniel+jessica@>
2019-07-16 14:22:20 -0700

    add first passing test

--- lib/numwords_logic.rb
+++ lib/numwords_logic.rb
@@ -1,1 +1,24 @@
 require('pry')
+
+class NumberConverter
+  def initialize(number)
+    @number = number
+  end
+
+  # def numwordList
+  #
+  # end
+
+  def wordResponse
+    @list = {
+    1 => "one", 2 => "two", 3 => "three", 4 => "four", 5 => "five", 6 => "six", 7 => "svennness", 8 => "eight", 9 => "nine", 10 =>"ten"
+    }
+    @word_array = []
+    if @number == 8
+      @word_array.push(@list.fetch(8))
+    end
+    binding.pry
+    print @word_array
+    @word_array[0]
+  end
+end
--- spec/numwords_logic_spec.rb
+++ spec/numwords_logic_spec.rb
@@ -2,8 +2,10 @@ require('rspec')
 require('numwords_logic')
 require('pry')
 
-describe('Scrabble') do
-  it('') do
-    expect().to(eq())
+describe('NumberConverter') do
+  it('takes a number from the user and returns it in words') do
+    user_input = NumberConverter.new(8)
+    expect(user_input.wordResponse).to(eq(
+      "eight"))
   end
 end
